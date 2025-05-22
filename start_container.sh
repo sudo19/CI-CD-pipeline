1f99a502a6c7925333737ca61b3f7d622f6a94df
Sonal Dandhare <110013957+sudo19@users.noreply.github.com>
2025-05-22 09:34:21 +0530

    Create start_container.sh
    The is my shell file to run pipeline.

---
+++ start_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+echo
+
+# Run the Docker image as a container
+echo
+
+7.stop_container.sh
+#!/bin/bash
+set -e
+
+# Stop the running container (if any)
+echo "Hi"
